e2019ab36712dcdf313473041966befd660aeb2f
Kanishk Gupta <kanishkgupta1234@gmail.com>
2021-08-11 12:07:16 +0530

    feat: Added home screen

--- assets/screens.ts
+++ assets/screens.ts
@@ -12,3 +12,16 @@ export const footerScreenOptions: {name:string,icon:string}[] = [
         icon: "user"
     }
 ];
+
+export const promotionImages: string[] =[
+    "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2021/7/28/eefb920e-f613-46e6-859a-ea61343912661627481545766-Denim.jpg",
+    "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/11/14/0524cbd5-f034-4155-9e3b-336c4e530ee41605363272658-Home---Levis.jpg",
+    "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2021/7/28/9f3448b5-342a-41a1-bbbc-46af4544d8561627481545731-Men---s-Casual.jpg",
+    "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/11/14/d5c17859-f366-4a65-ab42-5a066254feaa1605363272474-Home---GAP.jpg",
+    "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2021/7/28/6c54e558-5e7d-4edc-909e-49109a0f1d381627481545748-flats---heels.jpg",
+    "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/11/14/5544cd64-d95b-461d-802a-8025fdfeb3331605363272844-Home---Nike.jpg",
+    "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/581e2bf6-6d47-4a4b-a11f-43200dc9c6791598892519362-Puma.jpg",
+    "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/64ecc170-58af-473f-8144-b1639895a8291598892519457-Veromodo.jpg",
+    "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/retaillabs/2020/9/4/e6c7a718-acc0-4aa6-b04c-470ab139d66c1599230080722-F21--4-.jpg",
+    "https://assets.myntassets.com/f_webp,w_196,c_limit,fl_progressive,dpr_2.0/assets/images/2020/8/31/af31285e-f6a3-426e-bbea-0aedef9da17c1598892377537-Tommy-Hilfiger.jpg"
+]
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,18 +1,69 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, Text, SafeAreaView, Button, Alert, TouchableOpacity } from "react-native";
-import {  } from "react-native-elements/dist/buttons/Button";
+import { View,Text, SafeAreaView,TouchableOpacity, ScrollView, Image } from "react-native";
+import { Card } from "react-native-elements";
 import tw from "tailwind-react-native-classnames";
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Footer from "../components/Footer";
+import { promotionImages } from "../assets/screens";
+
 
 export default function HomeScreen() {
+    const navigation: any = useNavigation();
     return (
-        <SafeAreaView style={{display: "flex",flex:1, justifyContent: "center", alignItems: 'center'}}>
-        <View>
-            <Text>
-            Home page!!
+        <SafeAreaView style={{display: "flex",flex:1}}>
+        <ScrollView>
+            <Text style={tw`p-4 pb-0 text-base font-semibold`}>
+                Deal Of The Day
             </Text>
-        </View>
+                <View
+                style={tw`flex flex-row flex-wrap justify-evenly`}
+                >
+                   { promotionImages.slice(0,4).map((imgUrl)=>(
+                    <Card
+                    containerStyle={{
+                        width: 140
+                    }}
+                    >
+                        <TouchableOpacity
+                        onPress={()=>navigation.navigate("Product")}
+                        >
+                        <Image
+                        source={{uri: imgUrl}}
+                        style={{
+                            width: 100,
+                            height: 150
+                        }}
+                        />
+                        </TouchableOpacity>
+                    </Card>
+                ))}
+                </View>
+                <Text style={tw`p-4 pb-0 text-base font-semibold`}>
+                Top Brands
+                </Text>
+                <View
+                style={tw`flex flex-row flex-wrap pb-2 justify-evenly`}
+                >
+                   { promotionImages.slice(4,8).map((imgUrl)=>(
+                    <Card
+                    containerStyle={{
+                        width: 140
+                    }}
+                    >
+                        <TouchableOpacity
+                        onPress={()=>navigation.navigate("Product")}
+                        >
+                        <Image
+                        source={{uri: imgUrl}}
+                        style={{
+                            width: 100,
+                            height: 150
+                        }}
+                        />
+                        </TouchableOpacity>
+                    </Card>
+                ))}
+                </View>
+        </ScrollView>
         </SafeAreaView>
     )
 }
